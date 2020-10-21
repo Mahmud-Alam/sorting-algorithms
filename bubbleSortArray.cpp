25015fd6903805d318ecd4954b68451097c9f129
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-21 19:15:15 +0600

    UPDATE:(n-i-1)

--- bubbleSortArray.cpp
+++ bubbleSortArray.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int* bubbleSortArray(int n, int *arr){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-1;j++){
-            if(arr[j]>arr[j+1]){
+    for(int i=0;i<n-1;i++){               //-> Outer LOOP: no need to iterate till last element, we can iterate till (n-1).
+        for(int j=0;j<n-i-1;j++){        //-> Inner LOOP: hence, last element will be maximum after every inner loopping,
+            if(arr[j]>arr[j+1]){        //thats why we don't need iterate inner loop till (n-1), we can reduce it by (n-i-1);
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
