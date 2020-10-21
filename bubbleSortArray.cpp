14dd70b243e2b30ec841c638a62a2e8a9923ce0c
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-21 18:58:09 +0600

    bubble sort in array

--- bubbleSortArray.cpp
+++ bubbleSortArray.cpp
@@ -1,7 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void displayArray(int n,int arr[]){
+int* bubbleSortArray(int n, int *arr){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+void displayArray(int n,int *arr){
     cout<<endl<<"Array list: ";
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
@@ -17,4 +30,6 @@ int main(){
         cin>>arr[i];
     }
     displayArray(n,arr);
+    cout<<endl<<"After Bubble Sort"<<endl;
+    displayArray(n,bubbleSortArray(n,arr));
 }
